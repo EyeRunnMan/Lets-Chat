dda0be3120e8eeb78c41528d8cd28cb776f722ef
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-12-29 21:07:33 +0530

    input bar component created

---
+++ lets-chat-firebase/src/components/MessageInputBar/MessageInputBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./message-input-bar.scss";
+
+const MessageInputBar = ({ theme = "dark" }) => {
+   return (
+      <div className={`input-bar ${theme}-chat-window-bg`}>
+         <input className={`input-bar__field ${theme}-msg-input-field`} />
+         <button className="input-bar__button">
+            <i className={`input-bar__icon-send ${theme}-msg-input-icons`}></i>
+         </button>
+         <button className="input-bar__button">
+            <i
+               className={`input-bar__icon-delete ${theme}-msg-input-icons`}
+            ></i>
+         </button>
+      </div>
+   );
+};
+
+export default MessageInputBar;

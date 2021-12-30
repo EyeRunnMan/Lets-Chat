cc58ed64e099c132484228c798f7454c85a97b36
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-12-30 16:37:05 +0530

    settings component comleted

--- lets-chat-firebase/src/containers/ChatSettings/ChatSettings.js
+++ lets-chat-firebase/src/containers/ChatSettings/ChatSettings.js
@@ -3,23 +3,32 @@ import "./chat-settings.scss";
 
 const ChatSettings = ({ theme = "dark" }) => {
    return (
-      <div className={`chat-settings  ${theme}-chat-settings-bg`}>
-         <button className={`chat-settings__button ${theme}-chat-settings-btn`}>
-            <span>Save-Crypto-Keys</span>
-            <i
-               className={`chat-settings__icon-save ${theme}-chat-settings-btn-icon`}
-            ></i>
-         </button>
-         <button className={`chat-settings__button ${theme}-chat-settings-btn`}>
-            <span>Upload-Crypto-Keys</span>
-            <i
-               className={`chat-settings__icon-upload ${theme}-chat-settings-btn-icon`}
-            ></i>
-         </button>
-         <button className="chat-settings__button">
-            <span> Delete-Account</span>
-            <i className={`chat-settings__icon-delete`}></i>
-         </button>
+      <div className="chat-settings-wrapper">
+         <h4
+            className={`chat-settings-wrapper__heading ${theme}-section-heading`}
+         >{`//Settings`}</h4>
+         <div className={`chat-settings  ${theme}-chat-settings-bg`}>
+            <button
+               className={`chat-settings__button ${theme}-chat-settings-btn`}
+            >
+               <span>Save-Crypto-Keys</span>
+               <i
+                  className={`chat-settings__icon-save ${theme}-chat-settings-btn-icon`}
+               ></i>
+            </button>
+            <button
+               className={`chat-settings__button ${theme}-chat-settings-btn`}
+            >
+               <span>Upload-Crypto-Keys</span>
+               <i
+                  className={`chat-settings__icon-upload ${theme}-chat-settings-btn-icon`}
+               ></i>
+            </button>
+            <button className="chat-settings__button">
+               <span> Delete-Account</span>
+               <i className={`chat-settings__icon-delete`}></i>
+            </button>
+         </div>
       </div>
    );
 };
